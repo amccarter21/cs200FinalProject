755c627e099935497267ecd806aa610767af23dc
jerhansen <hans8517@bears.unco.edu>
2017-11-15 19:45:54 -0700

    tester 2

--- CS200FinalProject/src/Philadelphia_Roster.java
+++ CS200FinalProject/src/Philadelphia_Roster.java
@@ -3,4 +3,7 @@ public class Philadelphia_Roster extends Offense {
 // This is a tester
    Offense j = new Offense();
    //sike their offense sucks dick
+   
+   
+   // This is a second tester with the branch
 }
