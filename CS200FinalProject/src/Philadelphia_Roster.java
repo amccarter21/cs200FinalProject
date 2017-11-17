83d20a7fb0b16a8002486d19f379a71693fe1db1
jerhansen <hans8517@bears.unco.edu>
2017-11-17 10:33:53 -0700

    Secondary

--- CS200FinalProject/src/Philadelphia_Roster.java
+++
@@ -1,9 +0,0 @@
-
-public class Philadelphia_Roster extends Offense {
-// This is a tester
-   Offense j = new Offense();
-   //sike their offense sucks dick
-   
-   
-   // This is a second tester with the branch
-}
